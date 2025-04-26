511af1638dc755f8d7756eb9f257abc20cecf423
VaradScript <108749445+VaradScript@users.noreply.github.com>
2025-04-26 23:32:20 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import QuoteCard from './components/QuoteCard';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 to-pink-500">
+      <QuoteCard />
+    </div>
+  );
+}
